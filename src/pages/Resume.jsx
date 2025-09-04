f324458791459e169250b35b7910a9afb1cf6845
Jake Sales <jakesales@gmail.com>
2025-09-04 11:40:36 -0700

    Updated resume
    Added portfolio link (even though they are already on the portfolio site)

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ const Resume = () => {
     <div className="pt-20 px-6 max-w-5xl mx-auto">
       <h2 className="text-3xl font-bold mb-6 text-center">Resume</h2>
       <iframe
-        src="/GameDev-Portfolio/JakeSalesResume2025 (4).pdf"
+        src="/GameDev-Portfolio/JakeSalesResume2025.pdf"
         title="Jake Sales Resume"
         className="w-full h-[90vh] border rounded"
       />
